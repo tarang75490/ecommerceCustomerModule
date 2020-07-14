4a4630ef7b6b52b1f8b3d67e16343a9f36401bd7
tarang75490 <2018383@iiitdmj.ac.in>
2020-07-14 10:14:13 +0530

    Initial Commit With Folder Structures

---
+++ src/config/swagger.js
@@ -0,0 +1,19 @@
+exports.options = {
+  routePrefix: '/documentation',
+  exposeRoute: true,
+  swagger: {
+    info: {
+      title: 'Product Service APIs',
+      description: 'Product related all CRUD APIs',
+      version: '1.0.0',
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here'
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+  }
+}
